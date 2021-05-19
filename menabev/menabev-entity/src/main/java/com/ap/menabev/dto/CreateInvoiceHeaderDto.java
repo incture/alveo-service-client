cc290f4649968cb22552a215a5122b868d7ebfd1
Arun.Gauda <Arun.Gauda@INCTN282.Incture.local>
2021-05-19 12:27:20 +0530

    check push

--- menabev/menabev-entity/src/main/java/com/ap/menabev/dto/CreateInvoiceHeaderDto.java
+++ menabev/menabev-entity/src/main/java/com/ap/menabev/dto/CreateInvoiceHeaderDto.java
@@ -8,7 +8,6 @@ import lombok.Data;
 public class CreateInvoiceHeaderDto {
 	
 	private InvoiceHeaderDto invoiceHeaderDto;
-	private List<InvoiceItemDto>  invoiceItemDto;
 	private List<InvoiceItemAcctAssignmentDto> invoiceItemAcctAssignmentDto;
 	private List<CostAllocationDto> costAllocationDto;
 
