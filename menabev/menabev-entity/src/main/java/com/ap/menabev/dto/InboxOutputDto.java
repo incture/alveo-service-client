a5a3679b9318c112f9ebdd008959537be5e137c6
Arun.Gauda <Arun.Gauda@INCTN282.Incture.local>
2021-06-18 10:55:55 +0530

    inbox

--- menabev/menabev-entity/src/main/java/com/ap/menabev/dto/InboxOutputDto.java
+++ menabev/menabev-entity/src/main/java/com/ap/menabev/dto/InboxOutputDto.java
@@ -1,55 +1,27 @@
 package com.ap.menabev.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class InboxOutputDto {
-	
-	
 	private String requestId;
-
 	private String vendorName;
-	
-	private String compCode;
-
-	private Long refDocNum;
-
-	private String extInvNum;
-	// added for the tag in xml _InvoiceDate as discussed
+	private String extInvNum;// added for the tag in xml _InvoiceDate as discussed
 	private Long invoiceDate;
-	
 	private Long dueDate;
-
-	private String createdAt;
-
 	private String VendorId;
 	private String invoiceType;
 	private Long sapInvoiceNumber;
-	
-	private String taskStatus;
-	
-	private String invoiceTotalFrom;
-
-	private String invoiceTotalTo;
-
-	private String createdAtFrom;
-
-	private String createdAtTo;
-	
-	private Long dueDateFrom;
-	
-	private String dueDateTo;
-	
 	private  String validationStatus; 
-	
 	private String invoiceTotal;
-	
-	private boolean isClaimed;
-	
-	private String claimedUser;
-	
 	private String taskId;
-	
-	private int totalCount;
-
+	private String processor;
+	private String status;
+	private String claimedAt;
+	private String completedAt;
+	private String createdAt;
+	private List<String> recipientUsers;
+	private List<String> recipientGroups;
 }
