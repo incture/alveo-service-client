4994f185cc50a94f6ee600e453579717d701dc02
Lakhu D <Lakhu D@INC01426>
2021-06-21 14:33:45 +0530

    Activity Log Tables  - Lakhu

--- menabev/menabev-entity/src/main/java/com/ap/menabev/dto/ActivityLogDto.java
+++ menabev/menabev-entity/src/main/java/com/ap/menabev/dto/ActivityLogDto.java
@@ -3,24 +3,38 @@ package com.ap.menabev.dto;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Column;
+
 import com.sap.db.jdbc.packet.LOB;
 
 import lombok.Data;
 
 @Data
 public class ActivityLogDto {
-	
-	
-	private String id;
+
+	private String guid = UUID.randomUUID().toString();
+
 	private String workflowTaskId;
 	private String requestId;
-	private String exceptionCode;
-	private String priority;
 	private String taskOwner;
-	private String statusOfException;
 	private String taskStatus;
-	private String taskUserId;
-	private String taskComments;
-	private Date createdAt;
+	private String workflowId;
+	private String workflowStatus;
+	private Long workflowCreatedAt;
+	private String workflowCreateBy;
+	private String taskId;
+	private Long taskCreatedAt;
+	private Long completedAt;
+	private String processor;// 255
+	private String activityId;// 20
+	private String actionCode;// 2
+	private String invoiceStatusCode;// 2
+	private String invoiceStatusText;// 50
+	private String commentsGuid;
+	private String attachmentGuid;
+	private Long createdAt;
+	private String createdBy;
+	private Long updatedAt;
+	private String updatedBy;
 
 }
--- menabev/menabev-entity/src/main/java/com/ap/menabev/entity/ActivityLogDo.java
+++ menabev/menabev-entity/src/main/java/com/ap/menabev/entity/ActivityLogDo.java
@@ -18,8 +18,8 @@ import lombok.Data;
 public class ActivityLogDo {
 
 @Id
-@Column(name = "ID")
-private String id = UUID.randomUUID().toString();
+@Column(name = "GUID")
+private String guid = UUID.randomUUID().toString();
 
 
 @Column(name = "WORKFLOW_TASK_ID")
@@ -28,25 +28,65 @@ private String workflowTaskId;
 @Column(name = "REQUEST_ID")
 private String requestId;
 
-@Column(name = "EXCEPTION_CODE")
-private String exceptionCode;
-
-@Column(name = "PRIORITY")
-private String priority;
-
 @Column(name = "TASK_OWNER")
 private String taskOwner;
 
-@Column(name = "STATUS_OF_EXCEPTION")
-private String statusOfException;
 
 @Column(name = "TASK_STATUS")
 private String taskStatus;
 
-@Column(name = "TASK_USER_ID")
-private String taskUserId;
+@Column(name = "WORKFLOW_ID")
+ private String workflowId;
 
-@Column(name = "TASK_COMMENTS")
-private String taskComments;
+@Column(name = "WORKFLOW_STATUS")
+private String workflowStatus;
+
+@Column(name = "WORKFLOW_CREATED_AT")
+private Long workflowCreatedAt;
+
+@Column(name = "WORKFLOW_CREATED_BY")
+private String workflowCreateBy;
+
+@Column(name = "TASK_ID")
+private String taskId;
+
+@Column(name = "TASK_CREATED_AT")
+private Long taskCreatedAt;
+
+@Column(name = "COMPLETED_AT")
+private Long completedAt;
+
+@Column(name = "PROCESSOR",length=255)
+private String processor;//255
+
+@Column(name = "ACTIVITY_ID",length=20)
+private String activityId;//20
+
+@Column(name = "ACTION_CODE",length=2)
+private String actionCode;//2
+
+@Column(name = "INVOICE_STATUS_CODE",length=2)
+private String invoiceStatusCode;//2
+
+@Column(name = "INVOICE_STATUS_TEXT",length=50)
+private String invoiceStatusText;//50
+
+@Column(name = "COMMENTS_GUID")
+private String commentsGuid;
+
+@Column(name = "ATTACHMENT_GUID")
+private String attachmentGuid;
+
+@Column(name = "CREATED_AT")
+private Long createdAt;
+
+@Column(name = "CREATED_BY")
+private String createdBy;
+
+@Column(name = "UPDATED_AT")
+private Long updatedAt;
+
+@Column(name = "UPDATED_BY")
+private String updatedBy;
 
 }
