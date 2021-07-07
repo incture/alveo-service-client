1f5dd07dc2db7d2de4865d2ba68a61c9f9ea0674
Incture@123 <Laxmi.Baragukar@incture.com>
2021-07-07 10:32:08 +0530

    comments removed from main class

--- menabev/menabev-web/src/main/java/com/ap/menabev/MenaBevApplication.java
+++ menabev/menabev-web/src/main/java/com/ap/menabev/MenaBevApplication.java
@@ -20,59 +20,20 @@ import com.ap.menabev.util.ApplicationConstants;
 @EnableAsync
 public class MenaBevApplication {
 
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(MenaBevApplication.class, args);
-		List<String> list= new ArrayList<>();
-		list.add("a");
-		list.add("e");
-		String response=formInputUrl(list);
-		System.out.println("response:"+response);
 	}
-	public static String formInputUrl(List<String> invoiceReferenceNumberList){
-		StringBuilder urlForm = new StringBuilder();
-		appendParamInOdataUrl(urlForm, "&$format","json" );
-		appendParamInOdataUrl(urlForm, "&$filter","" );
-		appendValuesInOdataUrl(urlForm,"ReferenceInvoiceNumber",invoiceReferenceNumberList);
-		//appendInOdataUrl(urlForm, ")and","(" );
-		urlForm.insert(0, ("/sap/opu/odata/sap/ZP2P_API_INVOICESTATUS_SRV/InvoiceStatusSet?"));
-		System.err.println("url"+urlForm.toString());
-		return urlForm.toString();
-		}
-	public static void appendValuesInOdataUrl(StringBuilder url,String key, List<String> value){
-		for(int i = 0; i<value.size();i++){
-			if(value.size()==1){
-			url.append(key+"%20eq%20"+"%27"+value.get(i)+"%27");
-			System.out.println("26 ");
-			}
-			else if(i==value.size()-1){
-				url.append(key+"%20eq%20"+"%27"+value.get(i)+"%27");
-				System.out.println("30 ");
-			}else{
-				url.append(key+"%20eq%20"+"%27"+value.get(i)+"%27"+"%20or%20");
-				System.out.println("29 ");
-				
-			}
-		}
-	}
-	public static void appendParamInOdataUrl(StringBuilder url, String key, String value) {
-		url.append( key + "=" + value);
-		}
-		public static void appendInOdataUrl(StringBuilder url, String key, String value) {
-		url.append( key + "" + value);
-		}
-		
+
 	@Bean
-	  public Executor taskExecutor() {
-	    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-	    executor.setCorePoolSize(2);
-	    executor.setMaxPoolSize(2);
-	    executor.setQueueCapacity(500);
-	    executor.setThreadNamePrefix("GithubLookup-");
-	    executor.initialize();
-	    return executor;
-	  }
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
+	}
 
 	@Bean
 	@Primary
@@ -84,7 +45,4 @@ public class MenaBevApplication {
 		dataSourceBuilder.password(ApplicationConstants.PASSWORD);
 		return dataSourceBuilder.build();
 	}
-
-	
-
 }
